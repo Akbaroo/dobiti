8d369799d6415de52f12078a812c4d34da514722
akbar <akbrg0872@gmail.com>
2025-04-08 17:58:02 -0700

    fix(blog): app name in blog

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
+app_name = 'blog'
 
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
