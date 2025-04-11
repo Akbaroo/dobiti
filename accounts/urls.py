96e7b8de59f499d1b4bd56039755385d9a66b931
akbar <akbrg0872@gmail.com>
2025-04-10 23:04:25 -0700

    fix(name)

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = 'blog'
+app_name = 'auth'
 
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
