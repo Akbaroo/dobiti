cbf6b498de7f36de755d93b2061d108ccd8c1596
akbar <akbrg0872@gmail.com>
2025-04-10 17:29:49 -0700

    feat(post): added fields (model) - jalali data - like - audio - video - image

--- blogs/models.py
+++ blogs/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 from persiantools.jdatetime import JalaliDateTime
+
+User = get_user_model()
 
 
 class Post(models.Model):
@@ -9,12 +12,21 @@ class Post(models.Model):
     content = models.TextField(verbose_name="محتوا")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ایجاد")
     jalali_created_at = models.CharField(
-        max_length=20, blank=True, null=True, validators=[RegexValidator(r'\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}')]
-        )
-    
+        max_length=20,
+        blank=True,
+        null=True,
+        validators=[RegexValidator(r"\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}")],
+    )
+    likes = models.ManyToManyField(
+        User, related_name="liked_posts", blank=True, verbose_name="لایک ها"
+    )
+    image = models.ImageField(upload_to="post_images/", null=True, blank=True)
+    video = models.FileField(upload_to="post_videos/", null=True, blank=True)
+    audio = models.FileField(upload_to="post_audio/", null=True, blank=True)
+
     class Meta:
-        verbose_name = 'پست'
-        verbose_name_plural = 'پست '
+        verbose_name = "پست"
+        verbose_name_plural = "پست "
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
@@ -24,13 +36,16 @@ class Post(models.Model):
             )
         super().save(*args, **kwargs)
 
+    def total_likes(self):
+        return self.likes.count()
+
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, models.CASCADE, verbose_name=_('پست'))
+    post = models.ForeignKey(Post, models.CASCADE, verbose_name=_("پست"))
     text = models.TextField(_("متن"))
-    
+
     def __str__(self):
-        return f'{self.post.title} - {self.pk} نظر'
+        return f"{self.post.title} - {self.pk} نظر"
