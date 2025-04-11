4cfc30d564237110340f6bee7ae2a585bb19c4b6
akbar <akbrg0872@gmail.com>
2025-04-10 17:34:34 -0700

    fix(settings): for media - url - dir - gitignore

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 
 
 
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -10,3 +11,7 @@ urlpatterns = [
     path("auth/", include('accounts.urls')),
     path("", include('blogs.urls')),
 ]
+
+# اضافه کردن آدرس فایل‌های رسانه‌ای
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
