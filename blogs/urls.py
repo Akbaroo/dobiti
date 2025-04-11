434fb577a011506205540761ee4498879675fcdc
akbar <akbrg0872@gmail.com>
2025-04-10 17:36:13 -0700

    feat(post): - post detail view - like view

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'blogs'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('post/<int:post_id>/', views.post_detail_view, name='post_detail'),
+    path('post/<int:post_id>/like/', views.like_post_view, name='like_post'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from .models import Post
 
+
 def home(request):
-    posts = Post.objects.order_by('-created_at')  # جدیدترین اول
-    return render(request, 'home.html', {'posts': posts})
+    posts = Post.objects.order_by("-created_at")
+    return render(request, "home.html", {"posts": posts})
+
+
+def post_detail_view(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, "blogs/post_detail.html", {"post": post})
+
+
+
+@require_POST
+@login_required
+def like_post_view(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    user = request.user
+
+    if user in post.likes.all():
+        post.likes.remove(user)
+        liked = False
+    else:
+        post.likes.add(user)
+        liked = True
+
+    return JsonResponse({
+        'liked': liked,
+        'total_likes': post.likes.count()
+    })
