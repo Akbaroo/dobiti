09a51d6085d0dd60ac7544c7ac317e69d7449232
akbar <akbrg0872@gmail.com>
2025-04-13 22:19:44 -0700

    refactor(blog): refactor post detail template and form comment

--- blogs/forms.py
+++ blogs/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from .models import Comment
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['text']
+        fields = ["text"]
         widgets = {
-            'text': forms.Textarea(attrs={'rows': 3, 'placeholder': 'نظر خود را وارد کنید...'})
+            "text": forms.Textarea(
+                attrs={
+                    "rows": 3,
+                    "placeholder": "کامنت خود را وارد کنید...",
+                    "class": "form-control",
+                    "id": "exampleFormControlTextarea1",
+                }
+            )
         }
